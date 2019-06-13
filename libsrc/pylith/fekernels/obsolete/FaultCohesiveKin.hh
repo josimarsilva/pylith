7c1fe78e9185a0dbb23bf743098894da88a22423
Josimar <josimar@mit.edu>
2019-06-12 20:31:19 -0600

    Added the obsolete files from the Hackaton for easy referencing

---
+++ libsrc/pylith/fekernels/obsolete/FaultCohesiveKin.hh
@@ -0,0 +1,290 @@
+/* -*- C++ -*-
+ *
+ * ----------------------------------------------------------------------
+ *
+ * Brad T. Aagaard, U.S. Geological Survey
+ * Charles A. Williams, GNS Science
+ * Matthew G. Knepley, University of Chicago
+ *
+ * This code was developed as part of the Computational Infrastructure
+ * for Geodynamics (http:*geodynamics.org).
+ *
+ * Copyright (c) 2010-2015 University of California, Davis
+ *
+ * See COPYING for license information.
+ *
+ * ----------------------------------------------------------------------
+ */
+
+/** @file libsrc/fekernels/FaultCohesiveKin.hh
+ *
+ * Kernels for faults with prescribed slip.
+ *
+ * Solution fields: [disp(dim), vel(dim, optional), lagrange(dim)]
+ *
+ * Auxiliary fields: [...] (not used)
+ *
+ * LHS Residual: no contribution
+ *
+ * RHS Residual
+ *
+ *  - g0u^+ = -\lambda
+ *  - g0u^- = +\lambda
+ *  - g0\lambda = d - u^+ + u^-
+ *
+ * LHS Jacobian: no contribution
+ *
+ * RHS Jacobian
+ *
+ *  - Jg0^{u \lambda} = -1 for u^+, +1 for u^-
+ *  - Jg0^{\lambda u} = -1 for u^+, +1 for u^-
+ *
+ * ======================================================================
+ */
+
+#if !defined(pylith_fekernels_faultcohesivekin_hh)
+#define pylith_fekernels_faultcohesivekin_hh
+
+// Include directives ---------------------------------------------------
+#include "fekernelsfwd.hh" // forward declarations
+
+#include "pylith/utils/types.hh"
+
+class pylith::fekernels::FaultCohesiveKin {
+
+    // PUBLIC MEMBERS ///////////////////////////////////////////////////////
+public:
+
+    /** Kernel interface.
+     *
+     * @param[in] dim Spatial dimension.
+     * @param[in] numS Number of registered subfields in solution field.
+     * @param[in] numA Number of registered subfields in auxiliary field.
+     * @param[in] sOff Offset of registered subfields in solution field [numS].
+     * @param[in] sOff_x Offset of registered subfields in gradient of the solution field [numS].
+     * @param[in] s Solution field with all subfields.
+     * @param[in] s_t Time derivative of solution field.
+     * @param[in] s_x Gradient of solution field.
+     * @param[in] aOff Offset of registered subfields in auxiliary field [numA]
+     * @param[in] aOff_x Offset of registered subfields in gradient of auxiliary field [numA]
+     * @param[in] a Auxiliary field with all subfields.
+     * @param[in] a_t Time derivative of auxiliary field.
+     * @param[in] a_x Gradient of auxiliary field.
+     * @param[in] t Time for residual evaluation.
+     * @param[in] x Coordinates of point evaluation.
+     * @param[in] numConstants Number of registered constants.
+     * @param[in] constants Array of registered constants.
+     * @param[out] f0 [dim].
+     */
+
+
+    /** g0 function for elasticity equation: g0u = -\lambda.
+     *
+     * Solution fields: [disp(dim), ..., lagrange(dim)]
+     */
+    static
+    void g0u_pos(const PylithInt dim,
+                 const PylithInt numS,
+                 const PylithInt numA,
+                 const PylithInt sOff[],
+                 const PylithInt sOff_x[],
+                 const PylithScalar s[],
+                 const PylithScalar s_t[],
+                 const PylithScalar s_x[],
+                 const PylithInt aOff[],
+                 const PylithInt aOff_x[],
+                 const PylithScalar a[],
+                 const PylithScalar a_t[],
+                 const PylithScalar a_x[],
+                 const PylithReal t,
+                 const PylithScalar x[],
+                 const PylithInt numConstants,
+                 const PylithScalar constants[],
+                 PylithScalar g0[]);
+
+
+    /** g0 function for elasticity equation: g0u = +\lambda.
+     *
+     * Solution fields: [disp(dim), ..., lagrange(dim)]
+     */
+    static
+    void g0u_neg(const PylithInt dim,
+                 const PylithInt numS,
+                 const PylithInt numA,
+                 const PylithInt sOff[],
+                 const PylithInt sOff_x[],
+                 const PylithScalar s[],
+                 const PylithScalar s_t[],
+                 const PylithScalar s_x[],
+                 const PylithInt aOff[],
+                 const PylithInt aOff_x[],
+                 const PylithScalar a[],
+                 const PylithScalar a_t[],
+                 const PylithScalar a_x[],
+                 const PylithReal t,
+                 const PylithScalar x[],
+                 const PylithInt numConstants,
+                 const PylithScalar constants[],
+                 PylithScalar g0[]);
+
+
+    /** g0 function for slip constraint equation: g0\lambda = 0.5*d - u^+.
+     *
+     * Solution fields: [disp(dim), ..., lagrange(dim)]
+     */
+    static
+    void g0l_pos(const PylithInt dim,
+                 const PylithInt numS,
+                 const PylithInt numA,
+                 const PylithInt sOff[],
+                 const PylithInt sOff_x[],
+                 const PylithScalar s[],
+                 const PylithScalar s_t[],
+                 const PylithScalar s_x[],
+                 const PylithInt aOff[],
+                 const PylithInt aOff_x[],
+                 const PylithScalar a[],
+                 const PylithScalar a_t[],
+                 const PylithScalar a_x[],
+                 const PylithReal t,
+                 const PylithScalar x[],
+                 const PylithInt numConstants,
+                 const PylithScalar constants[],
+                 PylithScalar g0[]);
+
+
+    /** g0 function for slip constraint equation: g0\lambda = 0.5*d + u^-.
+     *
+     * Solution fields: [disp(dim), ..., lagrange(dim)]
+     */
+    static
+    void g0l_neg(const PylithInt dim,
+                 const PylithInt numS,
+                 const PylithInt numA,
+                 const PylithInt sOff[],
+                 const PylithInt sOff_x[],
+                 const PylithScalar s[],
+                 const PylithScalar s_t[],
+                 const PylithScalar s_x[],
+                 const PylithInt aOff[],
+                 const PylithInt aOff_x[],
+                 const PylithScalar a[],
+                 const PylithScalar a_t[],
+                 const PylithScalar a_x[],
+                 const PylithReal t,
+                 const PylithScalar x[],
+                 const PylithInt numConstants,
+                 const PylithScalar constants[],
+                 PylithScalar g0[]);
+
+
+    /** Jg0 function for displacement equation: -\lambda.
+     *
+     * Solution fields: [disp(dim), ..., lagrange(dim)]
+     */
+    static
+    void Jg0ul_pos(const PylithInt dim,
+                   const PylithInt numS,
+                   const PylithInt numA,
+                   const PylithInt sOff[],
+                   const PylithInt sOff_x[],
+                   const PylithScalar s[],
+                   const PylithScalar s_t[],
+                   const PylithScalar s_x[],
+                   const PylithInt aOff[],
+                   const PylithInt aOff_x[],
+                   const PylithScalar a[],
+                   const PylithScalar a_t[],
+                   const PylithScalar a_x[],
+                   const PylithReal t,
+                   const PylithReal s_tshift,
+                   const PylithScalar x[],
+                   const PylithInt numConstants,
+                   const PylithScalar constants[],
+                   PylithScalar Jg0[]);
+
+
+    /** Jg0 function for displacement equation: +\lambda.
+     *
+     * Solution fields: [disp(dim), ..., lagrange(dim)]
+     */
+    static
+    void Jg0ul_neg(const PylithInt dim,
+                   const PylithInt numS,
+                   const PylithInt numA,
+                   const PylithInt sOff[],
+                   const PylithInt sOff_x[],
+                   const PylithScalar s[],
+                   const PylithScalar s_t[],
+                   const PylithScalar s_x[],
+                   const PylithInt aOff[],
+                   const PylithInt aOff_x[],
+                   const PylithScalar a[],
+                   const PylithScalar a_t[],
+                   const PylithScalar a_x[],
+                   const PylithReal t,
+                   const PylithReal s_tshift,
+                   const PylithScalar x[],
+                   const PylithInt numConstants,
+                   const PylithScalar constants[],
+                   PylithScalar Jg0[]);
+
+
+    /** Jg0 function for slip constraint equation: -\lambda.
+     *
+     * Solution fields: [disp(dim), ..., lagrange(dim)]
+     */
+    static
+    void Jg0lu_pos(const PylithInt dim,
+                   const PylithInt numS,
+                   const PylithInt numA,
+                   const PylithInt sOff[],
+                   const PylithInt sOff_x[],
+                   const PylithScalar s[],
+                   const PylithScalar s_t[],
+                   const PylithScalar s_x[],
+                   const PylithInt aOff[],
+                   const PylithInt aOff_x[],
+                   const PylithScalar a[],
+                   const PylithScalar a_t[],
+                   const PylithScalar a_x[],
+                   const PylithReal t,
+                   const PylithReal s_tshift,
+                   const PylithScalar x[],
+                   const PylithInt numConstants,
+                   const PylithScalar constants[],
+                   PylithScalar Jg0[]);
+
+
+    /** Jg0 function for slip constraint equation: +\lambda.
+     *
+     * Solution fields: [disp(dim), ..., lagrange(dim)]
+     */
+    static
+    void Jg0lu_neg(const PylithInt dim,
+                   const PylithInt numS,
+                   const PylithInt numA,
+                   const PylithInt sOff[],
+                   const PylithInt sOff_x[],
+                   const PylithScalar s[],
+                   const PylithScalar s_t[],
+                   const PylithScalar s_x[],
+                   const PylithInt aOff[],
+                   const PylithInt aOff_x[],
+                   const PylithScalar a[],
+                   const PylithScalar a_t[],
+                   const PylithScalar a_x[],
+                   const PylithReal t,
+                   const PylithReal s_tshift,
+                   const PylithScalar x[],
+                   const PylithInt numConstants,
+                   const PylithScalar constants[],
+                   PylithScalar Jg0[]);
+
+
+}; // FaultCohesiveKin
+
+#endif // pylith_fekernels_faultcohesivekin_hh
+
+
+/* End of file */
---
+++ libsrc/pylith/fekernels/obsolete/Poroelasticity.hh
@@ -0,0 +1,190 @@
+/* -*- C++ -*-
+ *
+ * ----------------------------------------------------------------------
+ *
+ * Brad T. Aagaard, U.S. Geological Survey
+ * Charles A. Williams, GNS Science
+ * Matthew G. Knepley, University of Chicago
+ *
+ * This code was developed as part of the Computational Infrastructure
+ * for Geodynamics (http:*geodynamics.org).
+ *
+ * Copyright (c) 2010-2015 University of California, Davis
+ *
+ * See COPYING for license information.
+ *
+ * ----------------------------------------------------------------------
+ */
+
+/** @file libsrc/fekernels/Poroelasticity.hh
+ *
+ * Generic elasticity pointwise functions
+ *
+ */
+
+#if !defined(pylith_fekernels_poroelasticity_hh)
+#define pylith_fekernels_poroelasticity_hh
+
+// Include directives ---------------------------------------------------
+#include "fekernelsfwd.hh" // forward declarations
+
+#include "pylith/utils/types.hh"
+
+class pylith::fekernels::Poroelasticity {
+
+    // PUBLIC MEMBERS ///////////////////////////////////////////////////////
+public:
+
+    /** Kernel interface.
+     *
+     * @param[in] dim Spatial dimension.
+     * @param[in] numS Number of registered subfields in solution field.
+     * @param[in] numA Number of registered subfields in auxiliary field.
+     * @param[in] sOff Offset of registered subfields in solution field [numS].
+     * @param[in] sOff_x Offset of registered subfields in gradient of the solution field [numS].
+     * @param[in] s Solution field with all subfields.
+     * @param[in] s_t Time derivative of solution field.
+     * @param[in] s_x Gradient of solution field.
+     * @param[in] aOff Offset of registered subfields in auxiliary field [numA]
+     * @param[in] aOff_x Offset of registered subfields in gradient of auxiliary field [numA]
+     * @param[in] a Auxiliary field with all subfields.
+     * @param[in] a_t Time derivative of auxiliary field.
+     * @param[in] a_x Gradient of auxiliary field.
+     * @param[in] t Time for residual evaluation.
+     * @param[in] x Coordinates of point evaluation.
+     * @param[in] numConstants Number of registered constants.
+     * @param[in] constants Array of registered constants.
+     * @param[out] Storage coefficient at constant strain.
+     */
+    /** g0 function for generic poroelasticity terms (gravitational body force).
+     *
+     * g0_Poroelasticity: \vec{g0} = \vec{f}(t)
+     *
+     * Solution fields: []
+     * Auxiliary fields: [density, gravity_field(dim)]
+     */
+    static
+    void g0v_grav(const PylithInt dim,
+                  const PylithInt numS,
+                  const PylithInt numA,
+                  const PylithInt sOff[],
+                  const PylithInt sOff_x[],
+                  const PylithScalar s[],
+                  const PylithScalar s_t[],
+                  const PylithScalar s_x[],
+                  const PylithInt aOff[],
+                  const PylithInt aOff_x[],
+                  const PylithScalar a[],
+                  const PylithScalar a_t[],
+                  const PylithScalar a_x[],
+                  const PylithReal t,
+                  const PylithScalar x[],
+                  const PylithInt numConstants,
+                  const PylithScalar constants[],
+                  PylithScalar g0[]);
+
+
+    /** g0 function for generic poroelasticity terms (body force).
+     *
+     * g0_Poroelasticity: \vec{g0} = \vec{f}(t)
+     *
+     * Auxiliary fields: [body_force(dim)]
+     */
+    static
+    void g0v_bodyforce(const PylithInt dim,
+                       const PylithInt numS,
+                       const PylithInt numA,
+                       const PylithInt sOff[],
+                       const PylithInt sOff_x[],
+                       const PylithScalar s[],
+                       const PylithScalar s_t[],
+                       const PylithScalar s_x[],
+                       const PylithInt aOff[],
+                       const PylithInt aOff_x[],
+                       const PylithScalar a[],
+                       const PylithScalar a_t[],
+                       const PylithScalar a_x[],
+                       const PylithReal t,
+                       const PylithScalar x[],
+                       const PylithInt numConstants,
+                       const PylithScalar constants[],
+                       PylithScalar g0[]);
+
+    // ----------------------------------------------------------------------
+    // g0p function for generic poroelasticity terms (source density).
+    static
+    void g0p_source(const PylithInt dim,
+                  const PylithInt numS,
+                  const PylithInt numA,
+                  const PylithInt sOff[],
+                  const PylithInt sOff_x[],
+                  const PylithScalar s[],
+                  const PylithScalar s_t[],
+                  const PylithScalar s_x[],
+                  const PylithInt aOff[],
+                  const PylithInt aOff_x[],
+                  const PylithScalar a[],
+                  const PylithScalar a_t[],
+                  const PylithScalar a_x[],
+                  const PylithReal t,
+                  const PylithScalar x[],
+                  const PylithInt numConstants,
+                  const PylithScalar constants[],
+                  PylithScalar g0p_source[]);
+
+    // ----------------------------------------------------------------------
+    // f0p function for generic poroelasticity terms (source density).
+    static
+    void f0p_couple(const PylithInt dim,
+                  const PylithInt numS,
+                  const PylithInt numA,
+                  const PylithInt sOff[],
+                  const PylithInt sOff_x[],
+                  const PylithScalar s[],
+                  const PylithScalar s_t[],
+                  const PylithScalar s_x[],
+                  const PylithInt aOff[],
+                  const PylithInt aOff_x[],
+                  const PylithScalar a[],
+                  const PylithScalar a_t[],
+                  const PylithScalar a_x[],
+                  const PylithReal t,
+                  const PylithScalar x[],
+                  const PylithInt numConstants,
+                  const PylithScalar constants[],
+                  PylithScalar f0p_couple[]);
+
+
+    /** M function for storage at constant strain. Fluid volume released per unit drop in pressure
+     *
+     * \vec{f0} = \frac{(\alpha-\phi}{K} + \frac{\phi}{K_f}
+     *
+     * Auxiliary fields: [bulkModulus(2), porosity(4), fluidBulkModulus(7), biotCoefficient(8)]
+     */
+    /*
+    static
+    void mstorage(const PylithInt dim,
+                     const PylithInt numS,
+                     const PylithInt numA,
+                     const PylithInt sOff[],
+                     const PylithInt sOff_x[],
+                     const PylithScalar s[],
+                     const PylithScalar s_t[],
+                     const PylithScalar s_x[],
+                     const PylithInt aOff[],
+                     const PylithInt aOff_x[],
+                     const PylithScalar a[],
+                     const PylithScalar a_t[],
+                     const PylithScalar a_x[],
+                     const PylithReal t,
+                     const PylithScalar x[],
+                     const PylithInt numConstants,
+                     const PylithScalar constants[],
+                     PylithReal storageCoefficientStrain);
+     */
+
+}; // Elasticity
+
+#endif // pylith_fekernels_poroelasticity_hh
+
+// End of file
