732f57f103508aa2c42f66cf5ef28117e65201b6
Robert Walker <rlwalker@usc.edu>
2019-07-23 13:24:09 -0700

    bug fixes

--- libsrc/pylith/fekernels/IsotropicLinearPoroelasticity.hh
+++ libsrc/pylith/fekernels/IsotropicLinearPoroelasticity.hh
@@ -202,7 +202,7 @@ void Jf0pe(const PylithInt dim,
 * Auxiliary fields: [density(1), shear_modulus(1), bulk_modulus(1), other poroelastic related param ...]
 */
 static
-void g1p_grav(const PylithInt dim,
+void g1p_Grav(const PylithInt dim,
              const PylithInt numS,
              const PylithInt numA,
              const PylithInt sOff[],
